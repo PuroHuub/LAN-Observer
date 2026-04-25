2a233a149ccc53803becc21ccef009bf68226f0a
PuroHuub <thay.uar719@gmail.com>
2026-04-25 18:57:55 +0200

    5VersionFixed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import logger_config
-import reader
-import validator
 import logging
-import writer
-import scanner
-import writer_user
 import start
 
 logger = logging.getLogger(__name__)
@@ -17,76 +12,28 @@ ascii_snek = """\
               `'--....--'`
 """
 
-def find_ipv4():
-    writer.get_ip()
-    
-    readed_ipaddress = reader.read_file("discovered_targets.txt")
-    logger.debug(f"Getted data: {readed_ipaddress}")
-    
-    validated_data = validator.validate_ip_addresses(readed_ipaddress)
-    print(f"{validated_data}")
-    
-    #scanner.check_host('192.168.1.1')
-    #scanner.check_host('127.0.0.1')
-    
-    scannered_ipv = scanner.checking(validated_data['valid'])
-    
-    print(scannered_ipv)
-    
-def write_ipv4():
-    list_ipv4 = []
-    
-    while True:
-        input_ipv = input("введите 1 айпи или напишите end чтобы закочить запись: ")
-        if input_ipv == "end":
-            break
-        else:
-            list_ipv4.append(input_ipv)
-            
-    print(list_ipv4)
-    
-    writer_user.write(list_ipv4, "writed_targets.txt")
-    
-    readed_ipaddress = reader.read_file("writed_targets.txt")
-    
-    validated_data = validator.validate_ip_addresses(readed_ipaddress)
-    
-    print(f"{validated_data}")
-    
-    scannered_ipv = scanner.checking(validated_data['valid'])
-    print(scannered_ipv)
-
-def update_ipv4():
-    pass #в разработке (навсегда)
-
-def read_ipv4():
-    readed_ipaddress = reader.read_file("writed_targets.txt")
-    
-    validated_data = validator.validate_ip_addresses(readed_ipaddress)
-    
-    print(f"{validated_data}")
-    
-    scannered_ipv = scanner.checking(validated_data['valid'])
-    print(scannered_ipv)
-    
-def read_file():
-    path = input("введите абсолютный путь до файла: ")
-    
-    readed_ipaddress = reader.read_file(path)
-    
-    validated_data = validator.validate_ip_addresses(readed_ipaddress)
-    
-    print(f"{validated_data}")
-    
-    scannered_ipv = scanner.checking(validated_data['valid'])
-    print(scannered_ipv)
 
 def main():
     logger_config.logging_setup()
     
     print(f"{ascii_snek}welcome to SnakeObserver")
     
-    start.starting()
+    command = start.starting()
+    
+    if command == "--findIPV4":
+        start.find_ipv4()
+    elif command == "--writeIPV4":
+        start.write_ipv4()
+    elif command == "--updateIPV4":
+        pass
+    elif command == "--readIPV4":
+        start.read_ipv4()
+    elif command == "--readFile":
+        start.read_file()
+    elif command == "--help":
+        start.help()
+    else:
+        print("неизвестная команда")
 
 if __name__ == "__main__":
     main()
--- start.py
+++ start.py
@@ -1,4 +1,8 @@
-import main
+import reader
+import validator
+import writer
+import scanner
+import writer_user
 
 def starting():
     print("выберите две доступные вам функции:")
@@ -8,22 +12,75 @@ def starting():
     print("--readIPV4")
     print("--readFile")
     print("--help")
-    command = input()
     
-    if command == "--findIPV4":
-        main.find_ipv4()
-    elif command == "--writeIPV4":
-        main.write_ipv4()
-    elif command == "--updateIPV4":
-        pass
-    elif command == "--readIPV4":
-        main.read_ipv4()
-    elif command == "--readFile":
-        main.read_file()
-    elif command == "--help":
-        help()
-    else:
-        print("неизвестная команда")
+    return input().strip()
+    
         
 def help():
     print("--findIPV4 этой командой программа ищет все айпи в вашей LAN \n--writeIPV4 этой командой программа записывает ваши IPV4 \n--readIPV4 этой командой программа читает ранее записанные айпи \n--readFile этой командой программа читает айпишники из вашего файла путь котого вы указываете сами и не только название файла с абсолютным путем но и .txt указать должны")
+    
+    
+
+def find_ipv4():
+    writer.get_ip()
+    
+    readed_ipaddress = reader.read_file("discovered_targets.txt")
+    print(f"Getted data: {readed_ipaddress}")
+    
+    validated_data = validator.validate_ip_addresses(readed_ipaddress)
+    print(f"{validated_data}")
+    
+    #scanner.check_host('192.168.1.1')
+    #scanner.check_host('127.0.0.1')
+    
+    scannered_ipv = scanner.checking(validated_data['valid'])
+    
+    print(scannered_ipv)
+    
+def write_ipv4():
+    list_ipv4 = []
+    
+    while True:
+        input_ipv = input("введите 1 айпи или напишите end чтобы закочить запись: ")
+        if input_ipv == "end":
+            break
+        else:
+            list_ipv4.append(input_ipv)
+            
+    print(list_ipv4)
+    
+    writer_user.write(list_ipv4, "writed_targets.txt")
+    
+    readed_ipaddress = reader.read_file("writed_targets.txt")
+    
+    validated_data = validator.validate_ip_addresses(readed_ipaddress)
+    
+    print(f"{validated_data}")
+    
+    scannered_ipv = scanner.checking(validated_data['valid'])
+    print(scannered_ipv)
+
+def update_ipv4():
+    pass #в разработке (навсегда)
+
+def read_ipv4():
+    readed_ipaddress = reader.read_file("writed_targets.txt")
+    
+    validated_data = validator.validate_ip_addresses(readed_ipaddress)
+    
+    print(f"{validated_data}")
+    
+    scannered_ipv = scanner.checking(validated_data['valid'])
+    print(scannered_ipv)
+    
+def read_file():
+    path = input("введите абсолютный путь до файла: ")
+    
+    readed_ipaddress = reader.read_file(path)
+    
+    validated_data = validator.validate_ip_addresses(readed_ipaddress)
+    
+    print(f"{validated_data}")
+    
+    scannered_ipv = scanner.checking(validated_data['valid'])
+    print(scannered_ipv)
